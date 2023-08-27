7bfea5b1a1318343c5474880eff8efad192f7c2d
ISMAIL <ismailsabbr12@gmail.com>
2023-08-26 22:14:28 -0600

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const existingHeader = document.getElementById('victory');
+const newHeader = document.createElement('h1');
+const yourName = "Your Name";
+newHeader.id = 'victory';
+newHeader.textContent = `${yourName} is the champion`;
+existingHeader.parentNode.replaceChild(newHeader, existingHeader);
